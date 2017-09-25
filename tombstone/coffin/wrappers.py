0108dc584835abaea4184ba10e505bc24819bfee
Yufei Liu <yufeiliu94@gmail.com>
2017-09-25 08:28:18 +0800

    :bug::sparkles: fixed use_gpu && added set_log_level

--- tombstone/coffin/wrappers.py
+++ tombstone/coffin/wrappers.py
@@ -6,7 +6,14 @@ def use_cpu():
 def use_gpu(device):
     from caffe import set_mode_gpu, set_device
     set_mode_gpu()
+    set_device(device)
 
 
 class Phase:
     from caffe import TRAIN, TEST
+
+
+def set_log_level(level):
+    from caffe import init_log
+    init_log(level)
+
